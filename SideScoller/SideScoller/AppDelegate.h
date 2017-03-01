fb8f15d045fe572fcbcbfaa696228281a5c4b6ea
melissa <xumeng919@gmail.com>
2017-03-01 15:56:26 -0500

    create SideScoller

---
+++ SideScoller/SideScoller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SideScoller
+//
+//  Created by xumeng on 17/3/1.
+//  Copyright © 2017年 xumeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SideScoller/SideScoller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SideScoller
+//
+//  Created by xumeng on 17/3/1.
+//  Copyright © 2017年 xumeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
