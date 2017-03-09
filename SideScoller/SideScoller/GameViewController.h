b88058a94f3e17a1180a51709e85443b8778b400
melissa <xumeng919@gmail.com>
2017-03-09 17:40:39 -0500

    UI design

---
+++ SideScoller/SideScoller/GameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController.h
+//  SideScoller
+//
+//  Created by xumeng on 17/3/9.
+//  Copyright © 2017年 xumeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
